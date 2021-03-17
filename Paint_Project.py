d7f9a7d8a50e156f14b1bcd8f94ea34a6bf71806
Claudia Chajon <53544970+claudiasofiaC@users.noreply.github.com>
2021-03-17 16:46:04 -0400

    pep8 formating

--- Paint_Project.py
+++ Paint_Project.py
@@ -1,75 +1,80 @@
 import cv2
 import numpy as np
 
-#frameWidth = 640
-#frameHeight = 480
-cap = cv2.VideoCapture(1)
-cap.set(3, 640)
-cap.set(4, 480)
-cap.set(10, 130)
+frame_width = 700
+frame_height = 700
 
-# h_min, s_min, v_min, h_max, s_max,v_max
-myColors = [[26, 82, 119, 179, 255, 255],  # hot pink
-            [28, 78, 0, 138, 255, 255],    # green
-            [226, 90, 0, 132, 255, 255],   # yellow
-            [22, 115, 0, 179, 255, 255],   # blue
-            [35, 70, 66, 157, 255, 255],   # purple
-            [0, 181, 234, 33, 255, 255]]   # orange
+cap = cv2.VideoCapture(0)
+cap.set(3, frame_width)
+cap.set(4, frame_height)
+cap.set(10, 150)  # brightness setting
 
-myColorValues = [[153, 51, 255],   # hot pink          #BGR in HSV
-                 [0, 255, 0],      # green
-                 [102, 255, 255],  # yellow
-                 [255, 0, 0],      # blue
-                 [255, 0, 127 ],   # purple
-                 [0, 128, 255]]    # orange
+# define colors to detect
+# requires min and max saturation values for each color
+colors = [[153, 92, 163, 179, 255, 255],  # hot pink
+          [64, 143, 101, 99, 255, 255],   # green
+          [103, 102, 0, 131, 224, 255],   # purple
+          [5, 23, 213, 55, 193, 255]]     # yellow
+
+# list of BGR colors for each HSV color
+# so that bounding bx point is same color as color/object detected
+color_values = [[211, 73, 244],  # hot pink
+                [51, 255, 128],  # green
+                [255, 47, 149],  # purple
+                [43, 246, 204]]  # yellow
+
+# list of points
+points = []
 
 
-myPoints = [] #[x, y, colorId]
-
-def findColors(img, myColors, myColorValues):
-    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+# function for finding colors
+def find_color(img, colors, color_values):
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     count = 0
-    newPoints = []
-    for color in myColors: # gives us 3 diff windows
+    new_points = []
+    for color in colors:
         lower = np.array(color[0:3])
         upper = np.array(color[3:6])
-        mask = cv2.inRange(imgHSV,lower,upper)
-        getContours(mask)
-        x, y = getContours(mask)
-        cv2.circle(imgResult, (x, y), 10, myColorValues[count], cv2.FILLED)
-        if x!=0 and y!=0:
-            newPoints.append([x, y, count])
-        count +=1
-        #cv2.imshow(str(color[0]),mask)
-    return newPoints
+        mask = cv2.inRange(img_hsv, lower, upper)  # set limits for mask
+        x, y = get_contours(mask)
+        cv2.circle(img_result, (x, y), 10, color_values[count], cv2.FILLED)
+        if x != 0 and y != 0:
+            new_points.append([x, y, count])
+        count += 1
+        # cv2.imshow(str(color[0]), mask) for testing
+    return new_points
+
 
 # we need to get our contours, so that we can find the location of the object in img/vid
-def getContours(img):
+def get_contours(img):
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     x, y, w, h = 0, 0, 0, 0
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area>500:
-            #cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+        if area > 500:
+            # cv2.draw_contours(img_result, cnt, -1, (255, 0, 0), 3)
             peri = cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
             x, y, w, h = cv2.boundingRect(approx)
-    return x+w//2, y  # top of bounding box
+    return x+w//2, y  # top of bounding box at the tip of marker
 
-def drawOnCanvas(myPoints, myColorValues):
-    for point in myPoints:
-        cv2.circle(imgResult, (point[0], point[1]), 10, myColorValues[point[2]], cv2.FILLED)
+
+def draw_on_screen(points, color_values):
+    for point in points:
+        cv2.circle(img_result, (point[0], point[1]), 20, color_values[point[2]], cv2.FILLED)
+
 
 while True:
     success, img = cap.read()
-    imgResult = img.copy()
-    newPoints = findColors(img, myColors, myColorValues)
-    if len(newPoints) != 0:
-        for newP in newPoints:
-            myPoints.append(newP) # appendin since newpoints is a list
-    if len(myPoints) != 0:
-        drawOnCanvas(myPoints, myColorValues)
+    img_result = img.copy()
+    new_points = find_color(img, colors, color_values)
+    if len(new_points) != 0:
+        for newP in new_points:
+            points.append(newP)  # putting a list inside a list, since the points create a list
+    if len(points) != 0:
+        draw_on_screen(points, color_values)
 
-    cv2.imshow('Result', imgResult)
+    cv2.imshow("Result", img_result)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
